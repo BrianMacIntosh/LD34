02234d07c7609693355d88d66538515f6ee485c5
Lucas Colvin <lucas@zakainc.com>
2015-12-12 01:06:16 -0800

    draw the tiles

--- script_game/tileManager.js
+++ script_game/tileManager.js
@@ -2,21 +2,36 @@
 
 var terainKey = ["neutral", "sandy", "stone", "wood", "iron", "food", "water", "villageHall","rockBlock"]
 var growthKey = ["clear", "light", "medium", "heavy"]
-var startingTileGroupIndex = 3 //impies map total size
+var centerLTileIndex = 3 //impies map total size
 var lTileSize = 30
-var center = (startingTileGroupIndex*lTileSize)+Math.floor(lTileSize/2)-1
+var center = (centerLTileIndex*lTileSize)+Math.floor(lTileSize/2)-1
 
 tileManager = function(){
 	this.largeTileRows = []
-	for(var i = 0; i<(startingTileGroupIndex*2)+1; i++){
+	for(var i = 0; i<(centerLTileIndex*2)+1; i++){
 		this.largeTileRows[i] = []
 	}
-	this.largeTileRows[startingTileGroupIndex][startingTileGroupIndex] = genStartingTileGroup()
+	this.largeTileRows[centerLTileIndex][centerLTileIndex] = genStartingTileGroup(centerLTileIndex, centerLTileIndex)
 }
 
-var tile = function (terrainType, growthLevel){
+tileManager.textures =
+[
+	THREE.ImageUtils.loadTexture("media/dirt.png"),
+	THREE.ImageUtils.loadTexture("media/sand.png")
+]
+
+tileManager.geo = bmacSdk.GEO.makeSpriteGeo(64, 45);
+
+
+var tile = function (terrainType, growthLevel, globalX, globalY){
 	this.terrainType = ((terrainType != null) ? terrainType : 0);
 	this.growthLevel = ((growthLevel != null) ? growthLevel : 3);
+	
+	if(tileManager.textures[terrainType] != null && globalX != null){
+		var m = bmacSdk.GEO.makeSpriteMesh(tileManager.textures[terrainType], tileManager.geo);
+		m.position.set(globalX*64, globalY*45, -90);
+		GameEngine.scene.add(m);
+	}
 }
 
 //a 30x30 group of tiles
@@ -27,22 +42,22 @@ var genTileGroup = function(lTileX,lTileY){
 	}
 	if(lTileX == 0){ //block west
 		for(var y = 0; y<lTileSize; y++){
-			tiles[0][y] = new tile(8, 3);
+			tiles[0][y] = new tile(8, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 		}
 	} 
-	if(lTileX == startingTileGroupIndex*2){ //block west
+	if(lTileX == centerLTileIndex*2){ //block west
 		for(var y = 0; y<lTileSize; y++){
-			tiles[lTileSize-1][y] = new tile(8, 3);
+			tiles[lTileSize-1][y] = new tile(8, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 		}
 	}
-	if(lTileY == 0){ //block south
+	if(lTileY == 0){ //block north
 		for(var x = 0; x<lTileSize; x++){
-			tiles[x][0] = new tile(8, 3);
+			tiles[x][0] = new tile(8, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 		}
 	}
-	if(lTileY == startingTileGroupIndex*2){ //block north
+	if(lTileY == centerLTileIndex*2){ //block south
 		for(var x = 0; x<lTileSize; x++){
-			tiles[x][lTileSize-1] = new tile(8, 3);
+			tiles[x][lTileSize-1] = new tile(8, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 		}
 	}
 
@@ -51,19 +66,19 @@ var genTileGroup = function(lTileX,lTileY){
 			if(tiles[i][j] == null){ //fill tiles that are still empty
 				switch (Math.randomInt((lTileSize*lTileSize)/3)) { //on average 3 of each resource tile should be added to each tileGroup
 				  case 0:
-				  	tiles[i][j] = new tile(5, 3);
+				  	tiles[i][j] = new tile(5, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				    break;
 				  case 1:
-				  	tiles[i][j] = new tile(3, 3);
+				  	tiles[i][j] = new tile(3, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				    break;
 				  case 2:
-				  	tiles[i][j] = new tile(2, 3);
+				  	tiles[i][j] = new tile(2, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				    break;
 				  case 3:
-				  	tiles[i][j] = new tile(4, 3);
+				  	tiles[i][j] = new tile(4, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				    break;
 				  default:
-					tiles[i][j] = new tile();
+					tiles[i][j] = new tile(0,3,,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				    break;
 				}
 			}
@@ -72,34 +87,34 @@ var genTileGroup = function(lTileX,lTileY){
 	return tiles;
 }
 
-var genStartingTileGroup = function(){
+var genStartingTileGroup = function(lTileX,lTileY){
 	tiles = []
 	for(var i = 0; i<lTileSize; i++){
 		tiles[i] = []
 	}
 
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(5, 3); //food
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(5, 3); //food
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(5, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //food
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(5, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //food
 
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(3, 3); //wood
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(3, 3); //wood
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(3, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //wood
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(3, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //wood
 
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(2, 3);	//stone
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(2, 3);	//stone
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(2, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);	//stone
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(2, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);	//stone
 
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(4, 3); //iron
-	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(4, 3); //iron
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(4, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //iron
+	tiles[Math.randomInt(lTileSize)][Math.randomInt(lTileSize)] = new tile(4, 3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center); //iron
 
 
 	for(var i = 0; i<lTileSize; i++){
 		for(var j = 0; j<lTileSize; j++){
 			if(tiles[i][j] == null){ //fill tiles that are still empty
 				if((i==15||i==14)&&(j==14||j==15)){
-					tiles[i][j] = new tile(7, 0);
-				} else if ((i>9&&i<19)&&(j>9&&j<19)){
-					tiles[i][j] = new tile(0, 0);
+					tiles[i][j] = new tile(7, 0,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
+				} else if ((i>10&&i<19)&&(j>10&&j<19)){
+					tiles[i][j] = new tile(0, 0,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 				} else
-					tiles[i][j] = new tile();
+					tiles[i][j] = new tile(Math.randomInt(2),3,(lTileX*lTileSize+i)-center,(lTileY*lTileSize+j)-center);
 			}
 		}
 	}
@@ -111,7 +126,7 @@ tileManager.prototype.getTile = function(x, y){ // external 0,0 is largeTileRows
 	var lTileY = Math.floor((center+y)/lTileSize)
 	var sTileX = (center+x)%lTileSize
 	var sTileY = (center+y)%lTileSize
-	if(lTileX<0||lTileY<0||lTileX>=(startingTileGroupIndex*2)+1||lTileY>=(startingTileGroupIndex*2)+1){
+	if(lTileX<0||lTileY<0||lTileX>=(centerLTileIndex*2)+1||lTileY>=(centerLTileIndex*2)+1){
 		return "Here be Dragons. You've gone off the map."
 	}
 	if(this.largeTileRows[lTileX][lTileY] == null){
